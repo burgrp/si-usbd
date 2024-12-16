8c6454ffbb0d20842e676559fffc6a8b830c66f6
Pavel Burgr <pb@drake.cz>
2024-12-16 18:04:00 +0100

    formatting

--- usb-control-endpoint.cpp
+++ usb-control-endpoint.cpp
@@ -1,213 +1,224 @@
 namespace usbd {
 
-/**
- * @brief USB control endpoint implementation class.
- *
- * This class does not need to be subclassed. It may be used as is as member of
- * UsbDevice subclass.
- *
- * Implements standard device requests. And forwards vendor specific requests to
- * device, interfaces and endpoints.
- *
- */
-class UsbControlEndpoint : public UsbEndpoint {
-  int addressToSet;
+  /**
+   * @brief USB control endpoint implementation class.
+   *
+   * This class does not need to be subclassed. It may be used as is as member of
+   * UsbDevice subclass.
+   *
+   * Implements standard device requests. And forwards vendor specific requests to
+   * device, interfaces and endpoints.
+   *
+   */
+  class UsbControlEndpoint : public UsbEndpoint {
+    int addressToSet;
 
-public:
-  unsigned char rxBuffer[64];
-  unsigned char txBuffer[64];
+  public:
+    unsigned char rxBuffer[64];
+    unsigned char txBuffer[64];
 
-  void init() {
-    transferType = CONTROL;
-    rxBufferPtr = rxBuffer;
-    rxBufferSize = sizeof(rxBuffer);
-    txBufferPtr = txBuffer;
-    txBufferSize = sizeof(txBuffer);
-    UsbEndpoint::init();
-  }
-
-  void txComplete() {
-    if (addressToSet) {
-      device->setAddress(addressToSet);
-      addressToSet = 0;
+    void init() {
+      transferType = CONTROL;
+      rxBufferPtr = rxBuffer;
+      rxBufferSize = sizeof(rxBuffer);
+      txBufferPtr = txBuffer;
+      txBufferSize = sizeof(txBuffer);
+      UsbEndpoint::init();
     }
-  }
 
-  void setup(SetupData *setupData) {
+    void txComplete() {
+      if (addressToSet) {
+        device->setAddress(addressToSet);
+        addressToSet = 0;
+      }
+    }
 
-    if (setupData->bmRequestType.recipient == DEVICE) {
+    void setup(SetupData* setupData) {
 
-      if (setupData->bmRequestType.type == STANDARD) {
+      if (setupData->bmRequestType.recipient == DEVICE) {
 
-        if (setupData->bRequest == STD_REQUEST_GET_DESCRIPTOR) {
+        if (setupData->bmRequestType.type == STANDARD) {
 
-          int descriptorType = setupData->wValue >> 8;
-          int descriptorIndex = setupData->wValue & 0xFF;
+          if (setupData->bRequest == STD_REQUEST_GET_DESCRIPTOR) {
 
-          if (descriptorType == DESCRIPTOR_TYPE_DEVICE) {
+            int descriptorType = setupData->wValue >> 8;
+            int descriptorIndex = setupData->wValue & 0xFF;
 
-            DeviceDescriptor *deviceDesriptor = (DeviceDescriptor *)txBufferPtr;
-            zeromem(deviceDesriptor, sizeof(DeviceDescriptor));
+            if (descriptorType == DESCRIPTOR_TYPE_DEVICE) {
 
-            deviceDesriptor->bLength = sizeof(DeviceDescriptor);
-            deviceDesriptor->bDescriptorType = DESCRIPTOR_TYPE_DEVICE;
-            deviceDesriptor->bcdUSB = 0x200;
-            deviceDesriptor->bMaxPacketSize0 = rxPacketSize;
-            deviceDesriptor->iManufacturer = StringDescriptorID::MANUFACTURER;
-            deviceDesriptor->iProduct = StringDescriptorID::PRODUCT;
-            deviceDesriptor->iSerialNumber = StringDescriptorID::SERIAL;
+              DeviceDescriptor* deviceDesriptor = (DeviceDescriptor*)txBufferPtr;
+              zeromem(deviceDesriptor, sizeof(DeviceDescriptor));
 
-            deviceDesriptor->bNumConfigurations = 1;
+              deviceDesriptor->bLength = sizeof(DeviceDescriptor);
+              deviceDesriptor->bDescriptorType = DESCRIPTOR_TYPE_DEVICE;
+              deviceDesriptor->bcdUSB = 0x200;
+              deviceDesriptor->bMaxPacketSize0 = rxPacketSize;
+              deviceDesriptor->iManufacturer = StringDescriptorID::MANUFACTURER;
+              deviceDesriptor->iProduct = StringDescriptorID::PRODUCT;
+              deviceDesriptor->iSerialNumber = StringDescriptorID::SERIAL;
 
-            device->checkDescriptor(deviceDesriptor);
+              deviceDesriptor->bNumConfigurations = 1;
 
-            startTx(setupData->wLength < sizeof(DeviceDescriptor) ? setupData->wLength : sizeof(DeviceDescriptor));
+              device->checkDescriptor(deviceDesriptor);
 
-          } else if (descriptorType == DESCRIPTOR_TYPE_CONFIGURATION) {
+              startTx(setupData->wLength < sizeof(DeviceDescriptor) ? setupData->wLength : sizeof(DeviceDescriptor));
 
-            int totalLength = 0;
+            }
+            else if (descriptorType == DESCRIPTOR_TYPE_CONFIGURATION) {
 
-            ConfigurationDescriptor *configurationDescriptor = (ConfigurationDescriptor *)(txBuffer + totalLength);
-            zeromem(configurationDescriptor, sizeof(ConfigurationDescriptor));
-            configurationDescriptor->bLength = sizeof(ConfigurationDescriptor);
-            configurationDescriptor->bDescriptorType = DESCRIPTOR_TYPE_CONFIGURATION;
-            configurationDescriptor->bConfigurationValue = 1;
-            configurationDescriptor->bmAttributes.reservedSetTo1 = 1;
-            configurationDescriptor->bMaxPower = 50;
+              int totalLength = 0;
 
-            totalLength += sizeof(ConfigurationDescriptor);
+              ConfigurationDescriptor* configurationDescriptor = (ConfigurationDescriptor*)(txBuffer + totalLength);
+              zeromem(configurationDescriptor, sizeof(ConfigurationDescriptor));
+              configurationDescriptor->bLength = sizeof(ConfigurationDescriptor);
+              configurationDescriptor->bDescriptorType = DESCRIPTOR_TYPE_CONFIGURATION;
+              configurationDescriptor->bConfigurationValue = 1;
+              configurationDescriptor->bmAttributes.reservedSetTo1 = 1;
+              configurationDescriptor->bMaxPower = 50;
 
-            for (int i = 0; UsbInterface *interface = device->getInterface(i); i++) {
+              totalLength += sizeof(ConfigurationDescriptor);
 
-              configurationDescriptor->bNumInterfaces++;
+              for (int i = 0; UsbInterface * interface = device->getInterface(i); i++) {
 
-              InterfaceDescriptor *interfaceDescriptor = (InterfaceDescriptor *)(txBuffer + totalLength);
-              zeromem(interfaceDescriptor, sizeof(InterfaceDescriptor));
-              interfaceDescriptor->bLength = sizeof(InterfaceDescriptor);
-              interfaceDescriptor->bDescriptorType = DESCRIPTOR_TYPE_INTERFACE;
-              interfaceDescriptor->bInterfaceNumber = i;
-              interfaceDescriptor->bInterfaceClass = 0xFF;
-              interfaceDescriptor->iInterface = StringDescriptorID::INTERFACE_BASE + i;
+                configurationDescriptor->bNumInterfaces++;
 
-              interface->checkDescriptor(interfaceDescriptor);
-              totalLength += sizeof(InterfaceDescriptor);
+                InterfaceDescriptor* interfaceDescriptor = (InterfaceDescriptor*)(txBuffer + totalLength);
+                zeromem(interfaceDescriptor, sizeof(InterfaceDescriptor));
+                interfaceDescriptor->bLength = sizeof(InterfaceDescriptor);
+                interfaceDescriptor->bDescriptorType = DESCRIPTOR_TYPE_INTERFACE;
+                interfaceDescriptor->bInterfaceNumber = i;
+                interfaceDescriptor->bInterfaceClass = 0xFF;
+                interfaceDescriptor->iInterface = StringDescriptorID::INTERFACE_BASE + i;
 
-              int classDescriptorLength = interface->getClassDescriptorLength();
-              if (classDescriptorLength) {
-                unsigned char *classDescriptor = txBuffer + totalLength;
-                zeromem(classDescriptor, classDescriptorLength);
-                interface->checkClassDescriptor(classDescriptor);
-                totalLength += classDescriptorLength;
-              }
+                interface->checkDescriptor(interfaceDescriptor);
+                totalLength += sizeof(InterfaceDescriptor);
 
-              for (int e = 0; UsbEndpoint *endpoint = interface->getEndpoint(e); e++) {
-                for (int direction = 0; direction <= 1; direction++) {
+                int classDescriptorLength = interface->getClassDescriptorLength();
+                if (classDescriptorLength) {
+                  unsigned char* classDescriptor = txBuffer + totalLength;
+                  zeromem(classDescriptor, classDescriptorLength);
+                  interface->checkClassDescriptor(classDescriptor);
+                  totalLength += classDescriptorLength;
+                }
 
-                  int packetSize = direction ? endpoint->txPacketSize : endpoint->rxPacketSize;
+                for (int e = 0; UsbEndpoint * endpoint = interface->getEndpoint(e); e++) {
+                  for (int direction = 0; direction <= 1; direction++) {
 
-                  if (packetSize) {
+                    int packetSize = direction ? endpoint->txPacketSize : endpoint->rxPacketSize;
 
-                    interfaceDescriptor->bNumEndpoints++;
+                    if (packetSize) {
 
-                    EndpointDescriptor *endpointDescriptor = (EndpointDescriptor *)(txBuffer + totalLength);
-                    zeromem(endpointDescriptor, sizeof(EndpointDescriptor));
-                    endpointDescriptor->bLength = sizeof(EndpointDescriptor);
-                    endpointDescriptor->bDescriptorType = DESCRIPTOR_TYPE_ENDPOINT;
-                    endpointDescriptor->bEndpointAddress.direction = direction;
-                    endpointDescriptor->bEndpointAddress.index = endpoint->index;
-                    endpointDescriptor->bmAttributes.transferType = endpoint->transferType;
-                    endpointDescriptor->bmAttributes.synchronizationType = NO_SYNCHRONIZATION;
-                    endpointDescriptor->bmAttributes.usageType = DATA;
-                    endpointDescriptor->wMaxPacketSize = packetSize;
-                    endpointDescriptor->bInterval = endpoint->transferType == ISOCHRONOUS ? 1 : 10;
+                      interfaceDescriptor->bNumEndpoints++;
 
-                    endpoint->checkDescriptor(endpointDescriptor);
-                    totalLength += sizeof(EndpointDescriptor);
+                      EndpointDescriptor* endpointDescriptor = (EndpointDescriptor*)(txBuffer + totalLength);
+                      zeromem(endpointDescriptor, sizeof(EndpointDescriptor));
+                      endpointDescriptor->bLength = sizeof(EndpointDescriptor);
+                      endpointDescriptor->bDescriptorType = DESCRIPTOR_TYPE_ENDPOINT;
+                      endpointDescriptor->bEndpointAddress.direction = direction;
+                      endpointDescriptor->bEndpointAddress.index = endpoint->index;
+                      endpointDescriptor->bmAttributes.transferType = endpoint->transferType;
+                      endpointDescriptor->bmAttributes.synchronizationType = NO_SYNCHRONIZATION;
+                      endpointDescriptor->bmAttributes.usageType = DATA;
+                      endpointDescriptor->wMaxPacketSize = packetSize;
+                      endpointDescriptor->bInterval = endpoint->transferType == ISOCHRONOUS ? 1 : 10;
+
+                      endpoint->checkDescriptor(endpointDescriptor);
+                      totalLength += sizeof(EndpointDescriptor);
+                    }
                   }
                 }
               }
+
+              configurationDescriptor->wTotalLength = totalLength;
+              startTx(setupData->wLength < totalLength ? setupData->wLength : totalLength);
+
             }
+            else if (descriptorType == DESCRIPTOR_TYPE_STRING) {
 
-            configurationDescriptor->wTotalLength = totalLength;
-            startTx(setupData->wLength < totalLength ? setupData->wLength : totalLength);
+              StringDescriptor* stringDescriptor = (StringDescriptor*)txBuffer;
+              stringDescriptor->bDescriptorType = DESCRIPTOR_TYPE_STRING;
+              stringDescriptor->bLength = 2;
 
-          } else if (descriptorType == DESCRIPTOR_TYPE_STRING) {
+              if (descriptorIndex == 0) {
 
-            StringDescriptor *stringDescriptor = (StringDescriptor *)txBuffer;
-            stringDescriptor->bDescriptorType = DESCRIPTOR_TYPE_STRING;
-            stringDescriptor->bLength = 2;
-
-            if (descriptorIndex == 0) {
-
-              stringDescriptor->unicodeData[0] = 0x0409;
-              stringDescriptor->bLength += 2;
-
-            } else {
-
-              const char *str =
-                  descriptorIndex == StringDescriptorID::MANUFACTURER
-                      ? device->getManufacturer()
-                      : descriptorIndex == StringDescriptorID::PRODUCT
-                            ? device->getProduct()
-                            : descriptorIndex == StringDescriptorID::SERIAL
-                                  ? device->getSerial()
-                                  : descriptorIndex >= StringDescriptorID::INTERFACE_BASE &&
-                                            device->getInterface(descriptorIndex - StringDescriptorID::INTERFACE_BASE)
-                                        ? device->getInterface(descriptorIndex - StringDescriptorID::INTERFACE_BASE)
-                                              ->getLabel()
-                                        : "";
-              if (!str) {
-                str = "";
-              }
-
-              for (int c = 0; str[c]; c++) {
-                stringDescriptor->unicodeData[c] = str[c];
+                stringDescriptor->unicodeData[0] = 0x0409;
                 stringDescriptor->bLength += 2;
-              }
-            }
-            startTx(setupData->wLength < stringDescriptor->bLength ? setupData->wLength : stringDescriptor->bLength);
 
-          } else {
+              }
+              else {
+
+                const char* str =
+                  descriptorIndex == StringDescriptorID::MANUFACTURER
+                  ? device->getManufacturer()
+                  : descriptorIndex == StringDescriptorID::PRODUCT
+                  ? device->getProduct()
+                  : descriptorIndex == StringDescriptorID::SERIAL
+                  ? device->getSerial()
+                  : descriptorIndex >= StringDescriptorID::INTERFACE_BASE &&
+                  device->getInterface(descriptorIndex - StringDescriptorID::INTERFACE_BASE)
+                  ? device->getInterface(descriptorIndex - StringDescriptorID::INTERFACE_BASE)
+                  ->getLabel()
+                  : "";
+                if (!str) {
+                  str = "";
+                }
+
+                for (int c = 0; str[c]; c++) {
+                  stringDescriptor->unicodeData[c] = str[c];
+                  stringDescriptor->bLength += 2;
+                }
+              }
+              startTx(setupData->wLength < stringDescriptor->bLength ? setupData->wLength : stringDescriptor->bLength);
+
+            }
+            else {
+              startTx(0);
+            }
+
+          }
+          else if (setupData->bRequest == STD_REQUEST_SET_ADDRESS) {
+
+            addressToSet = setupData->wValue;
             startTx(0);
+
+          }
+          else if (setupData->bRequest == STD_REQUEST_GET_CONFIGURATION) {
+
+            txBuffer[0] = 1;
+            startTx(1);
+
+          }
+          else if (setupData->bRequest == STD_REQUEST_SET_CONFIGURATION) {
+
+            startTx(0);
+
+          }
+          else if (setupData->bRequest == STD_REQUEST_GET_STATUS) {
+
+            txBuffer[0] = 0;
+            txBuffer[1] = 0;
+            startTx(2);
           }
 
-        } else if (setupData->bRequest == STD_REQUEST_SET_ADDRESS) {
-
-          addressToSet = setupData->wValue;
-          startTx(0);
-
-        } else if (setupData->bRequest == STD_REQUEST_GET_CONFIGURATION) {
-
-          txBuffer[0] = 1;
-          startTx(1);
-
-        } else if (setupData->bRequest == STD_REQUEST_SET_CONFIGURATION) {
-
-          startTx(0);
-
-        } else if (setupData->bRequest == STD_REQUEST_GET_STATUS) {
-
-          txBuffer[0] = 0;
-          txBuffer[1] = 0;
-          startTx(2);
+        }
+        else {
+          device->setup(setupData);
         }
 
-      } else {
-        device->setup(setupData);
       }
-
-    } else if (setupData->bmRequestType.recipient == INTERFACE) {
-      UsbInterface *interface = device->getInterface(setupData->wIndex);
-      if (interface) {
-        interface->setup(setupData);
+      else if (setupData->bmRequestType.recipient == INTERFACE) {
+        UsbInterface* interface = device->getInterface(setupData->wIndex);
+        if (interface) {
+          interface->setup(setupData);
+        }
       }
-    } else if (setupData->bmRequestType.recipient == ENDPOINT) {
-      UsbEndpoint *endpoint = device->endpoints[setupData->wIndex & 0x0F];
-      if (endpoint && endpoint != this) {
-        endpoint->setup(setupData);
+      else if (setupData->bmRequestType.recipient == ENDPOINT) {
+        UsbEndpoint* endpoint = device->endpoints[setupData->wIndex & 0x0F];
+        if (endpoint && endpoint != this) {
+          endpoint->setup(setupData);
+        }
       }
     }
-  }
-};
+  };
 
 } // namespace usbd
--- usb-interface.cpp
+++ usb-interface.cpp
@@ -1,102 +1,102 @@
 namespace usbd {
 
-/**
- * @brief USB interface base class
- *
- * Interface usually has one or more endpoints.
- *
- * Typical subclass of UsbInterface class looks like:
- * ```cpp
- * class TestInterface : public usbd::UsbInterface {
- * public:
- *   TestEndpoint testEndpoint;
- *
- *   virtual UsbEndpoint *getEndpoint(int index) { return index == 0 ? &testEndpoint : NULL; }
- *
- * };
- * ```
- */
-class UsbInterface {
-public:
   /**
-   * Owner device.
-   * This property is set internally by the library.
+   * @brief USB interface base class
+   *
+   * Interface usually has one or more endpoints.
+   *
+   * Typical subclass of UsbInterface class looks like:
+   * ```cpp
+   * class TestInterface : public usbd::UsbInterface {
+   * public:
+   *   TestEndpoint testEndpoint;
+   *
+   *   virtual UsbEndpoint *getEndpoint(int index) { return index == 0 ? &testEndpoint : NULL; }
+   *
+   * };
+   * ```
    */
-  UsbDevice *device;
+  class UsbInterface {
+  public:
+    /**
+     * Owner device.
+     * This property is set internally by the library.
+     */
+    UsbDevice* device;
 
-  /**
-   * Interface initialization.
-   *
-   * Called internally from the library.
-   *
-   * UsbEndpoint::init() is responsible for calling init() method of endpoints.
-   */
-  virtual void init() {
-    for (int e = 0; UsbEndpoint *endpoint = getEndpoint(e); e++) {
-      endpoint->interface = this;
-      endpoint->device = device;
-      endpoint->init();
+    /**
+     * Interface initialization.
+     *
+     * Called internally from the library.
+     *
+     * UsbEndpoint::init() is responsible for calling init() method of endpoints.
+     */
+    virtual void init() {
+      for (int e = 0; UsbEndpoint * endpoint = getEndpoint(e); e++) {
+        endpoint->interface = this;
+        endpoint->device = device;
+        endpoint->init();
+      }
     }
-  }
 
-  /**
-   * Gets endpoint by index.
-   *
-   * Must be overloaded by the subclass.
-   *
-   * Called internally from the library.
-   *
-   * @param index zero based in scope of interface
-   * @return UsbEndpoint*
-   */
-  virtual UsbEndpoint *getEndpoint(int index) = 0;
+    /**
+     * Gets endpoint by index.
+     *
+     * Must be overloaded by the subclass.
+     *
+     * Called internally from the library.
+     *
+     * @param index zero based in scope of interface
+     * @return UsbEndpoint*
+     */
+    virtual UsbEndpoint* getEndpoint(int index) = 0;
 
-  /**
-   * Handler of interface control request.
-   *
-   * May be overloaded to implement vendor request.
-   *
-   * @param setupData
-   */
-  virtual void setup(SetupData *setupData){};
+    /**
+     * Handler of interface control request.
+     *
+     * May be overloaded to implement vendor request.
+     *
+     * @param setupData
+     */
+    virtual void setup(SetupData* setupData) {};
 
-  /**
-   * Called when generating interface descriptor.
-   *
-   * Called internally from the library.
-   *
-   * May be overloaded to modify the default descriptor.
-   *
-   * @param interfaceDesriptor
-   */
-  virtual void checkDescriptor(InterfaceDescriptor *interfaceDesriptor){};
+    /**
+     * Called when generating interface descriptor.
+     *
+     * Called internally from the library.
+     *
+     * May be overloaded to modify the default descriptor.
+     *
+     * @param interfaceDesriptor
+     */
+    virtual void checkDescriptor(InterfaceDescriptor* interfaceDesriptor) {};
 
-  /**
-   * Called when generating class descriptor.
-   *
-   * May be overloaded to add class specific descriptor.
-   *
-   * @return int
-   */
-  virtual int getClassDescriptorLength() { return 0; }
+    /**
+     * Called when generating class descriptor.
+     *
+     * May be overloaded to add class specific descriptor.
+     *
+     * @return int
+     */
+    virtual int getClassDescriptorLength() { return 0; }
 
-  /**
-   * Called when generating class descriptor.
-   *
-   * May be overloaded to add class specific descriptor.
-   *
-   * @param buffer
-   */
-  virtual void checkClassDescriptor(unsigned char *buffer) {}
+    /**
+     * Called when generating class descriptor.
+     *
+     * May be overloaded to add class specific descriptor.
+     *
+     * @param buffer
+     */
+    virtual void checkClassDescriptor(unsigned char* buffer) {}
 
-  /**
-   * Get string identifier of the interface.
-   *
-   * May be overloaded by the subclass.
-   *
-   * @return const char*
-   */
-  virtual const char *getLabel() { return NULL; };
-};
+    /**
+     * Get string identifier of the interface.
+     *
+     * May be overloaded by the subclass.
+     *
+     * @return const char*
+     */
+    virtual const char* getLabel() { return NULL; };
+  };
 
 } // namespace usbd
